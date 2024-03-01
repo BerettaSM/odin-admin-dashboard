8cb2ebca7427e97d0c820f74d8c78b665a493d39
BerettaSM <ramonsaviato@hotmail.com>
2024-02-29 23:13:41 -0300

    feat: add sidebar reset on resize

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,41 +3,55 @@ const sidebar = document.querySelector('.sidebar.mobile');
 const toggleButton = sidebar.querySelector('.menu-toggle');
 
 let lastFocusedElement;
-let focusRetentionCleanup;
+let resetFocusFn;
 
 window.addEventListener('focusin', function rememberLastFocus(event) {
     lastFocusedElement = event.relatedTarget;
 });
 
+window.addEventListener('resize', throttle(closeSidebar));
+
+toggleButton.addEventListener('click', function toggleSidebar() {
+    if (sidebar.classList.contains('open')) {
+        closeSidebar();
+    } else {
+        openSidebar();
+    }
+});
+
 function openSidebar() {
     sidebar.classList.add('open');
     document.body.classList.add('overlay-open');
-    toggleButton.querySelector('.visually-hidden').textContent = 'Close the sidebar menu';
+    toggleButton.querySelector('.visually-hidden').textContent =
+        'Close the sidebar menu';
 
-    focusRetentionCleanup = retainFocus(sidebar);
+    resetFocusFn = retainFocus(sidebar);
 }
 
 function closeSidebar() {
     sidebar.classList.remove('open');
     document.body.classList.remove('overlay-open');
-    toggleButton.querySelector('.visually-hidden').textContent = 'Open the sidebar menu';
+    toggleButton.querySelector('.visually-hidden').textContent =
+        'Open the sidebar menu';
 
-    if(typeof focusRetentionCleanup === 'function') {
-        focusRetentionCleanup();
-        focusRetentionCleanup = null;
+    if (typeof resetFocusFn === 'function') {
+        resetFocusFn();
+        resetFocusFn = null;
     }
 }
 
 function retainFocus(element) {
     const focusedElement = lastFocusedElement ?? document.body;
 
-    const focusableElements = element.querySelectorAll('a,button,input,textarea');
+    const focusableElements = element.querySelectorAll(
+        'a,button,input,textarea'
+    );
 
-    if(focusableElements.length > 0) {
+    if (focusableElements.length > 0) {
         window.addEventListener('focusin', onFocusChange);
         focusableElements[0].focus();
     }
-    
+
     return function cleanUpFunction() {
         window.removeEventListener('focusin', onFocusChange);
         focusedElement.focus();
@@ -49,30 +63,43 @@ function retainFocus(element) {
         const from = event.relatedTarget;
         const to = event.target;
 
-        const isFocusedElementWithinTarget = Array.prototype.indexOf.call(focusableElements, to) !== -1;
+        const isFocusedElementWithinTarget =
+            Array.prototype.indexOf.call(focusableElements, to) !== -1;
 
-        if(isFocusedElementWithinTarget) {
+        if (isFocusedElementWithinTarget) {
             // Focused element is within the element that is retaining focus, allow it.
             return;
         }
 
-        const hasFirstElementLostFocus = Array.prototype.indexOf.call(focusableElements, from) === 0;
+        const hasFirstElementLostFocus =
+            Array.prototype.indexOf.call(focusableElements, from) === 0;
 
-        if(hasFirstElementLostFocus) {
+        if (hasFirstElementLostFocus) {
             // Put focus back on last focusable element within the element.
             focusableElements[focusableElements.length - 1].focus();
         } else {
             // For all other cases of focus loss, put focus on first focusable element;
             focusableElements[0].focus();
         }
-
     }
 }
 
-toggleButton.addEventListener('click', function toggleSidebar() {
-    if(sidebar.classList.contains('open')) {
-        closeSidebar();
-    } else {
-        openSidebar();
+function throttle(func, ms = 500) {
+    var timestamp = getTimestamp();
+
+    return function throttled(...args) {
+        var now = getTimestamp();
+
+        if (now - timestamp < ms) {
+            return;
+        }
+
+        func(...args);
+
+        timestamp = getTimestamp();
+    };
+
+    function getTimestamp() {
+        return new Date().getTime();
     }
-});
+}
